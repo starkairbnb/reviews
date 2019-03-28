594e6fc9ea08ca3c646a20476033de100194487e
Calvin Yao <calvin.yao21@gmail.com>
2019-03-27 20:21:03 -0700

    [feat] random review seeding implemented

--- database/reviewGenerator.js
+++ database/reviewGenerator.js
@@ -3,7 +3,64 @@ const db = require('./index.js');
 const Model = require('./models.js');
 
 
-let ratings = [];
+const ratings = [];
+
+
+
+const reviews = [];
+const place = ['place', 'home', 'house', 'apartment', 'location', 'property'];
+const area = ['entrance', 'doorway', 'bedroom', 'bathroom', 'study', 'living room', 'basement', 'attic', 'closet', 'library', 'porch', 'yard', 'roof', 'kitchen', 'dining room', 'garage', 'backdoor', 'sidedoor'];
+const verbs = ['loved', 'enjoyed'];
+const adjectives = ['amazing', 'remarkable', 'the best', 'spectacular', 'outstanding', 'lovely', 'great', 'incomparable', 'pleasant', 'wonderful', 'incredible', 'marvelous', 'perfect'];
+const users = ['Rachael', 'Susan', 'Britney', 'Whitney', 'Jessica', 'Thinh', 'Katie', 'Tiffany', 'David', 'Marie', 'Sahar', 'Angela', 'Joe', 'Derrin', 'Michael', 'Alanna', 'Brian', 'Gina', 'Faris', 'Patricia', 'Ladonna', 'Brett', 'Nicole', 'Ben', 'Abby', 'Nessrin', 'Sheree'];
+const endingBlurb = ['Would recommend!', 'Very disappointed.', 'We loved our stay!', 'Charming!', 'I\'ll be back!', 'Thoroughly enjoyed every bit!', 'We cannot wait to go back!']
+const imageURls = [];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
+
+/*
+~280 count
+Good blend: 1/3 3-word reviews
+            1/3 longer, under 270-280 character reviews
+            1/3 over 270-280 character reviews
+
+            ...or not. 70% long, 30% short?
+*/
+function generateBody() {
+  let chance = Math.random();
+  if (chance > 0.7) {
+    return `This ${place[Math.floor(Math.random() * place.length)]} is absolutely ${adjectives[Math.floor(Math.random() * adjectives.length)]}! \
+You know it's a ${adjectives[Math.floor(Math.random() * adjectives.length)]} ${place[Math.floor(Math.random() * place.length)]} \
+when you don't want to do anything but be in the ${place[Math.floor(Math.random() * place.length)]} or go anywhere else. \
+This was the absolute ${adjectives[Math.floor(Math.random() * adjectives.length)]} ${place[Math.floor(Math.random() * place.length)]} \
+for what we were wanting. Obviously the view is enough to stand on its own, but we also ${verbs[Math.floor(Math.random() * verbs.length)]} \
+the little things like the ${area[Math.floor(Math.random() * area.length)]} \
+and just the overall feel of the ${place[Math.floor(Math.random() * place.length)]}. ${endingBlurb[Math.floor(Math.random() * endingBlurb.length)]}`;
+  } else {
+    return `This ${place[Math.floor(Math.random() * place.length)]} is ${adjectives[Math.floor(Math.random() * adjectives.length)]}! \
+${endingBlurb[Math.floor(Math.random() * endingBlurb.length)]}`;
+  }
+}
+
+// const template1 = `This ${place[Math.floor(Math.random() * place.length)]} is absolutely ${adjectives[Math.floor(Math.random() * adjectives.length)]}! \
+// You know it's a ${adjectives[Math.floor(Math.random() * adjectives.length)]} ${place[Math.floor(Math.random() * place.length)]} \
+// when you don't want to do anything but be in the ${place[Math.floor(Math.random() * place.length)]} or go anywhere else. \
+// This was the absolute ${adjectives[Math.floor(Math.random() * adjectives.length)]} ${place[Math.floor(Math.random() * place.length)]} \
+// for what we were wanting. Obviously the view is enough to stand on its own, but we also ${verbs[Math.floor(Math.random() * verbs.length)]} \
+// the little things like the ${area[Math.floor(Math.random() * area.length)]} \
+// and just the overall feel of the ${place[Math.floor(Math.random() * place.length)]}. ${endingBlurb[Math.floor(Math.random() * endingBlurb.length)]}`;
+// console.log(template1)
+
+// const template2 = `This ${place[Math.floor(Math.random() * place.length)]} is ${adjectives[Math.floor(Math.random() * adjectives.length)]}! \
+// ${endingBlurb[Math.floor(Math.random() * endingBlurb.length)]}`;
+// console.log(template2)
+
+  // `This place is absolutely amazing. You know it's a good place when you don't want to do anything but be in the place or go anywhere else. 
+  // This was the absolute perfect place for what we were wanting for our honeymoon. 
+  // We were able to decompress with the fresh air and view and the sound of birds every morning. 
+  // Obviously the view is enough to stand on its own, but we also loved the little things like picking of fresh fruit from the trees 
+  // and just the overall feel of the property. We cannot wait to go back!`
+
 for (let i = 0; i < 100; i++) {
   let ratingObj = {
     accuracy: Math.random() * 6,
@@ -34,40 +91,24 @@ for (let i = 0; i < 100; i++) {
   }
 
   ratings.push(ratingObj);
+
+  let reviewObj = {
+    user: users[Math.floor(Math.random() * users.length)],
+    date: `${months[Math.floor(Math.random() * months.length)]} ${years[Math.floor(Math.random() * years.length)]}`,
+    text: generateBody(),
+    imageURl: ''
+  };
+  reviews.push(reviewObj);
 }
 
-// console.log(ratings)
 
 
 Model.Rating.create(ratings)
   .then(() => {
     console.log('Ratings seeded');
+    return Model.Review.create(reviews);  
+  })
+  .then(() => {
+    console.log('Reviews seeded');
     db.close();
   })
-
-
-let reviews = [];
-let nouns = [];
-let verbs = ['Loved', 'Enjoyed'];
-let adjectives = [];
-let users = ['Rachael', 'Susan', 'Britney', 'Whitney', 'Jessica', 'Thinh', 'Katie', 'Tiffany', 'David', 'Marie', 'Sahar', 'Angela', 'Joe', 'Derrin', 'Michael', 'Alanna', 'Brian', 'Gina', 'Faris', 'Patricia', 'Ladonna', 'Brett', 'Nicole', 'Ben', 'Abby', 'Nessrin', 'Sheree'];
-let imageURls = [];
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'];
-let dates = [];
-/*
-~280 count
-Good blend: 1/3 3-word reviews
-            1/3 longer, under 270-280 character reviews
-            1/3 over 270-280 character reviews
-*/
-
-let longest = [
-  `This place is absolutely amazing. You know it's a good place when you don't want to do anything but be in the place or go anywhere else. 
-  This was the absolute perfect place for what we were wanting for our honeymoon. 
-  We were able to decompress with the fresh air and view and the sound of birds every morning. 
-  Obviously the view is enough to stand on its own, but we also loved the little things like picking of fresh fruit from the trees 
-  and just the overall feel of the property. We cannot wait to go back!`
-
-
-]

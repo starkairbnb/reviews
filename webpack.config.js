5c19c6cfa968423afc166514b7554a27e9028fef
Calvin Yao <calvin.yao21@gmail.com>
2019-04-01 11:06:01 -0700

    [ops] attempt to configure packages for custom font loading

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&importLoaders=true&localIdentName=[name]__[local]___[hash:base64:5]' 
+      },
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+        loader: 'url-loader?limit=100000',
+        include: [/fonts/]
       }
     ]
   },

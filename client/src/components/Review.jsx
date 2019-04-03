2803d9ac6ea94bfd6797ac86446ad280e9a93b11
Calvin Yao <calvin.yao21@gmail.com>
2019-04-03 14:59:36 -0700

    [feat] added 'Read More' states to long reviews

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -6,20 +6,25 @@ class Review extends React.Component {
     super(props);
     this.state = {
       long: false,
+      hovered: false,
       expanded: false,
       text1: '',
     }
-    this.expandText = this.expandText.bind(this)
+    this.expandText = this.expandText.bind(this);
+    this.readMoreHover = this.readMoreHover.bind(this);
   }
 
   componentDidMount() {
     const { text } = this.props.review;
-    console.log(text.length)
     if (text.length > 270 && text.substring(270).indexOf(' ') !== -1) {
       let space = 270 + text.substring(270).indexOf(' ');
       let text1 = text.substring(0, space);
       this.setState({ text1, long: true })
     }
+  }
+
+  readMoreHover() {
+    this.setState( { hovered: !this.state.hovered } )
   }
 
   expandText() {
@@ -36,17 +41,34 @@ class Review extends React.Component {
   //   text1 = text.substring(0, space) + '...';
   //   text2 = text.substring(space);
   // }
-  return (
-  <div>
-    <div className={review.frame}>
-     <img src={userImage} className={review.profilePic}/>
-    </div>
-    {user}<br/>
-    {date}<br/>
-    {/* {text} */}
-    {long ? (expanded ? <div>{text}</div> : <div>{text1 + '...'}<button type='button' className={review.readMore} onClick={this.expandText}>Read More</button></div>) : <div>{text}</div>}
-  </div>
-  )
+    return (
+      <div className={review.review}>
+        <div>
+          <img src={userImage} className={review.profilePic} />
+          <div className={review.postDetail}>
+            <div >
+              {user}<br />
+            </div>
+            <div >
+              {date}<br />
+            </div>
+          </div>
+        </div>
+        {long ? (expanded ?
+          <div>
+            {text}
+          </div> :
+          <div>
+            {text1 + '...'}
+            <button type='button' className={this.state.hovered ? review.readMoreHover : review.readMore} onMouseEnter={this.readMoreHover} onMouseLeave={this.readMoreHover} onClick={this.expandText}>
+              Read More
+            </button>
+          </div>) :
+          <div>
+            {text}
+          </div>}
+      </div>
+    )
   }
 }
 

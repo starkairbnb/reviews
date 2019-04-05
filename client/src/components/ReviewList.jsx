821e5d48504c7b02e12ca70a3745e45afa2ce2e2
Calvin Yao <calvin.yao21@gmail.com>
2019-04-04 21:44:28 -0700

    [prog] linked pagination component to review list

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import Review from './Review.jsx';
+// import ReactPaginate from 'react-paginate';
+// import paginate from '../css/paginate.css';
+import Pagination from './Pagination.jsx';
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      page: 1
     }
   }
 
-  render() { return (
-    <div>
-      {this.props.reviews.map((review, index) => {
-        return <Review key={index} review={review} />
-      })}
-      {/* <Review />
-      <Review />
-      <Review />
-      <Review />
-      <Review />
-      <Review />
-      <Review /> */}
+  render() { 
+    console.log(this.props)
+    return (
+    <div id='reviewList'>
+      <div>
+        {this.props.reviews.map((review, index) => {
+          return <Review key={index} review={review} />
+        })}
+      </div>
+      <div>
+        <Pagination pageCount={Math.ceil(this.props.count/7)} getReviews={this.props.getReviews}/>
+      </div>
     </div>
   )
   }

c77ca8bba20949d8e19e047b795eda3f8730c360
Calvin Yao <calvin.yao21@gmail.com>
2019-04-08 10:55:04 -0700

    [visual] finished css on header and put stars in div to work with flexbox

--- client/src/components/OverallSearch.jsx
+++ client/src/components/OverallSearch.jsx
@@ -13,28 +13,31 @@ class OverallSearch extends React.Component {
     let starArr = [];
     for (let i = 0; i < 5; i++) {
       let diff = this.props.average - i;
+      let starDiv = [];
       switch (true) {
         case diff >= 1:
-          starArr.push(<img className={overallSearch.fullStar} src="/images/fullStar.png" key={i.toString()} />)
+          starDiv.push(<img className={overallSearch.fullStar} src="/images/fullStar.png" key={i.toString()} />)
           break;
         case diff === 0.5:
-          starArr.push(<img className={overallSearch.halfStar} src="/images/halfStar.png" key={"half" + i.toString()} />)
+          starDiv.push(<img className={overallSearch.halfStar} src="/images/halfStar.png" key={"half" + i.toString()} />)
         case diff < 1:
-          starArr.push(<img className={overallSearch.emptyStar} src="/images/emptyStar.png" key={i.toString()} />)
+          starDiv.push(<img className={overallSearch.emptyStar} src="/images/emptyStar.png" key={i.toString()} />)
           break;
       }
+      starDiv = <div key={"div" + i}>{starDiv}</div>;
+      starArr.push(starDiv)
     }
 
     return (
       <div className={overallSearch.overallSearch}>
-        {/* <div> */}
-          <div>
+        <div className={overallSearch.header}>
+          <h2 className={overallSearch.reviewCount}>
             {this.props.count} Reviews
-          </div>
-          <div>
+          </h2>
+          <span className={overallSearch.starArr}>
             {starArr}
-          </div>
-        {/* </div> */}
+          </span>
+        </div>
         <div className={overallSearch.searchBox}>
           <img className={overallSearch.searchIcon} src="/images/search.png"/>
           <input className={overallSearch.searchInput} type='text' placeholder="Search Reviews" onSubmit={(e) => {e.target.preventDefault()}}/>
